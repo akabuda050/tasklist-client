4215708b40b0bbea7a506c3433e5a70cb9423e72
Olexandr Dolbin <olexandr.dolbin@gmail.com>
2023-03-17 22:09:04 +0100

    add shadow

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -165,7 +165,7 @@
           v-else
           class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-2 gap-4 text-gray-500 list-inside dark:text-gray-400"
         >
-          <li v-for="task in tasksFiltered" :key="`${task.id}`" class="h-[200px] p-3 border">
+          <li v-for="task in tasksFiltered" :key="`${task.id}`" class="h-[200px] p-3 border shadow">
             <div class="flex flex-col items-start">
               <div class="mb-2">
                 <div class="flex items-center mr-2 text-lg">
