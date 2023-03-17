3a7d79a1be35b7eb8f530b7aabf4005e94cce052
Olexandr Dolbin <olexandr.dolbin@gmail.com>
2023-03-17 22:41:10 +0100

    set min height

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -163,7 +163,7 @@
         </div>
         <ul
           v-else
-          class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-2 gap-4 text-gray-500 list-inside dark:text-gray-400"
+          class="min-h-[550px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-2 gap-4 text-gray-500 list-inside dark:text-gray-400"
         >
           <li v-for="task in tasksFiltered" :key="`${task.id}`" class="h-[200px] p-3 border shadow">
             <div class="flex flex-col items-start">
